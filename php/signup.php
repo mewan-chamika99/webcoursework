3351757589918a5c9d4e675ccb152d6995aa86df
unknown <mewan-chamika99>
2022-08-29 11:24:36 +0530

    complete php

--- php/signup.php
+++ php/signup.php
@@ -7,7 +7,22 @@
  	 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
   		exit();
 		}
-		echo"database connected"; 
+		$name =$_POST['uname'];
+		$password =$_POST['pass'];
+
+		//display username oassword
+		// echo "{$password} : {$name}";
+
+		//insert data into the DB
+		 $sql = "INSERT INTO goldenmewa(username, password) VALUES ('$name','$password')";
+
+			
+		 
+		if ($conn->query($sql) === TRUE) {
+		  echo "<br><br> you are signup successfully";
+		} else {
+		  echo "Error: " . $sql . "<br>" . $conn->error;
+		}
 	
 	$conn->close();
 ?>
